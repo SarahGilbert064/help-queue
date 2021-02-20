d338a47e60d6b0c0153c7572bf3b7186342cb575
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-02-20 11:32:36 -0800

    creat repo and add .gitignore

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import ticketsImage from "./../img/ticketReg.png";
+
+function Header(){
+  return (
+    <React.Fragment>
+      {/* <img src={ticketsImage} alt="An image of tickets" /> */}
+    </React.Fragment>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+// import reducer from './reducers/ticket-list-reducer';
+import rootReducer from './reducers/index';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
+// const store = createStore(reducer);
+
+store.subscribe(() =>
+  console.log(store.getState())
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
